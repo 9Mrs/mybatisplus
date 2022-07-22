c074de9afe756c914ce3356f8645d2156306780e
sjm <sjm@Lxx.com>
2022-07-22 19:07:00 +0800

    MybatisPlus 学习代码  merge hotfix commit

--- src/main/java/com/sjm/config/GitTest.java
+++ src/main/java/com/sjm/config/GitTest.java
@@ -3,6 +3,9 @@ package com.sjm.config;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("version 2.0");
+        System.out.println("master");
+        System.out.println("master");
+
         System.out.println("hot-fix");
         System.out.println("hotfix");
 
